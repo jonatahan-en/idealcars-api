40e32f45074e75e77edd64b75bda75af864137f6
jonatahan-es <jonatahan_enriquez@hotmail.com>
2025-04-10 15:58:35 +0200

    Añadir cambios en homeControllers.js

--- idealCars/app.js
+++ idealCars/app.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import connectMongoose from './lib/connectMongoose.js';
+import createError from 'http-errors';
+import logger from 'morgan';
 
+//conexion a la base de datos
 await connectMongoose()
 console.log("Conectado a mongoDB")
 const app = express();
+
+// crea un ana respuesta custon en el terminal
+app.use(logger("dev"));
 
 app.get("/", (req, res, next) => {
     res.send("Hello World!");
 });
 
-//conexion a la base de datos
+//catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404));
+});
 
 
 //error handler
