8564a63621aa793ef0e043be9a9c086f06cd15d7
jonatahan-es <jonatahan_enriquez@hotmail.com>
2025-04-15 15:46:58 +0200

    Eliminar carpeta controllers con minúscula del repositorio remoto

--- idealCars/controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-import Product from "../models/Products.js" 
-
-export async function index (req, res, next){
-    const userId = req.session.userId
-    res.locals.products = await Product.find() 
-    res.render("home")
-} 
-
