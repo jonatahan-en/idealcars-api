80ab0a5166a17c583f58d0ae5e25a9e7a754de9a
Jorge <barrosogalindojorge@gmail.com>
2025-04-30 18:46:00 +0200

    Validaciones basicas  mostrando errores parace que funcionan

--- idealCars/Controllers/productController.js
+++ idealCars/Controllers/productController.js
@@ -17,7 +17,8 @@ export function index(req, res, next) {
 
 
 export async function validateProduct(req, res, next) {
-    console.log(req.body)
+    console.log("Datos recibidos", req.body)
+    
     await body('name')
     .notEmpty().withMessage('El nombre es obligatorio')
     .isLength({min: 4}).withMessage('El nombre debe tener al menos 4 caracteres')
@@ -28,12 +29,12 @@ export async function validateProduct(req, res, next) {
     .isLength({min: 4}).withMessage('El modelo debe tener al menos 4 caracteres')
     .isAlpha('es-ES', { ignore: ' ' }).withMessage('El modelo solo puede contener letras')
     .run(req),
-    await  body('color')
+    await body('color')
     .notEmpty().withMessage('El color es obligatorio')
     .isLength({min: 3}).withMessage('El color debe tener al menos 3 caracteres')
     .isAlpha('es-ES', { ignore: ' ' }).withMessage('El color solo puede contener letras')
     .run(req),
-    await  body('year')
+    await body('year')
     .notEmpty().withMessage('El año es obligatorio')
     .isNumeric().withMessage('El año debe ser un número')
     .run(req),
@@ -43,13 +44,16 @@ export async function validateProduct(req, res, next) {
     .run(req),
     await body('kilometer')
     .notEmpty().withMessage('El kilometraje es obligatorio')
+    .isInt({min:0}).withMessage('El kilometraje no puede ser negativo')
     .isNumeric().withMessage('El kilometraje debe ser un número')
     .run(req)
+   
     
     const errors = validationResult(req)
     if(!errors.isEmpty()){
+        console.log("Errores de validación", errors.array())
         return res.render('new-product', {
-            errors: errors.array(),
+            errors: errors.mapped(),
                 name: req.body.name,
                 model: req.body.model,
                 color: req.body.color,
@@ -64,7 +68,8 @@ export async function validateProduct(req, res, next) {
 
 export async function postNew(req, res, next) {
     try {
-
+        console.log("Datos recibidos en postNew:", req.body);  // Verifica los datos recibidos
+        console.log("Archivo recibido:", req.file);  // Verifica si se ha subido correctamente un archivo de imagen
         const userId = req.session.userId
 
         const {name,model,color,year,price,kilometer,image} = req.body
@@ -79,10 +84,12 @@ export async function postNew(req, res, next) {
             image: req.file ? req.file.filename : null,
             owner: userId
         })
+        console.log("Producto a guardar:", product);
         await product.save()
         res.redirect('/')
         
     } catch (error) {
+        console.error("Error al guardar el producto:", error); 
         next(error)
     }
 }
